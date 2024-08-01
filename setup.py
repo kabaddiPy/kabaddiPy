7e90088f7d06009cb8feae4dc8410cc91aaf170f
annimukherjee <mukh.aniruddha.work@gmail.com>
2024-08-02 02:06:58 +0530

    restucturing

---
+++ setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kabaddi_data_aggregator",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Python module for aggregating Kabaddi data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/annimukherjee/kabaddi_data_aggregator",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "selenium",
+        # Add any other dependencies here
+    ],
+)
