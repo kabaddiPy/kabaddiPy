7286ecfc324bb5911fba54e9ee0b8ff510bc97dc
annimukherjee <mukh.aniruddha.work@gmail.com>
2024-08-02 01:53:07 +0530

    added_script

---
+++ main.py
@@ -0,0 +1,21 @@
+from kabaddi_data import KabaddiDataAggregator
+
+aggregator = KabaddiDataAggregator()
+
+# Get all team names
+team_names = aggregator.get_all_team_names()
+print("Team Names:", team_names)
+
+# # Get season standings
+# standings = aggregator.team_season_standings()
+# print("Season Standings:", standings)
+
+# Get player information
+players_info = aggregator.get_players_team_info_and_profile_url()
+print("Players Info:", players_info[:5])  # Print first 5 players
+
+# Get team line-up data
+line_up_data = aggregator.team_line_up()
+print("Team Line-up Data:", line_up_data)
+
+# Add more function calls as needed
---
+++ script.js
@@ -0,0 +1,8 @@
+function m() {
+	document.getElementById("download").click();
+	setTimeout(()=> {document.getElementsByClassName("frvoegc")[1].click();}, 2000);
+	setTimeout(()=> {document.getElementsByClassName("fc9tep5")[0].click();},5000);
+}
+
+
+m();
