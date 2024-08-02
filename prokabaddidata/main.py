e32a043d22f8bb00d6c9fd4be0e281716c6f15af
annimukherjee <mukh.aniruddha@gmail.com>
2024-08-03 04:36:24 +0530

    final-updates

--- prokabaddidata/main.py
+++ prokabaddidata/main.py
@@ -10,9 +10,6 @@ print("Team Names:", team_names)
 standings = aggregator.team_season_standings()
 print("Season Standings:", standings)
 
-# Get player information
-players_info = aggregator.get_players_team_info_and_profile_url()
-print("Players Info:", players_info[:5])  # Print first 5 players
 
 # Get team line-up data
 line_up_data = aggregator.team_line_up()
--- test.py
+++
@@ -1,7 +0,0 @@
-from prokabaddidata import main
-
-
-obj = p.KabaddiDataAggregator()
-
-print(obj.get_all_team_names())
-
