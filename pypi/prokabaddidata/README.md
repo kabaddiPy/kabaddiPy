8add1e5caa38f43b84d2ded6e16371b90ba0b572
Aniruddha Mukherjee <mukh.aniruddha@gmail.com>
2024-09-03 01:35:01 +0530

    major restructuring

--- pypi/prokabaddidata/README.md
+++ pypi/prokabaddidata/README.md
@@ -1,3 +1,17 @@
 ## Functions to Query Data
 
 
+We have the following types of data
+
+1. Match-Wise Data
+   1. Overview of each match
+   2. Play-by-Play data for each match
+2. Player-Wise Data
+   1. Various aggregated statistics for each player
+   2. Success rates for each raider on the basis of the Number of Defenders they're up against
+3. Standings for Various Seasons
+4. Team-Wise Data
+   1. Various aggregated statistics for each team
+   2. Aggregated statistics for each team on the basis of the Skills employed by their players for Tacking, Raiding, Defending and Counter Action
+
+
---
+++ pypi/prokabaddidata/prokabaddidata.py
@@ -0,0 +1,20 @@
+
+
+class KabaddiDataAPI:
+    
+    # for a season - display the standings
+    
+    # for a season -> display all the match ids for that season along with some basic info about the match (matches overview)
+    # for a given match-id -> get play-by-play data
+
+    
+
+    # for a given team in a given season return aggregated stats for that team
+    # for a given team in a season, return the defender skills and raider skills for that team
+
+    # return all matches played by that team
+
+
+    # get team roster with player-ids
+    # for a given player-id return aggregated stats about that player
+    
